d31b858f33c30007aa4c927ebd2f7935c94c7337
veronicatofino <veronicatofino@javerianacali.edu.co>
2020-05-26 19:26:03 -0500

    Adición IgnoreDataMember a atributo Products

--- PetShop-Api/Models/CategoryModel.cs
+++ PetShop-Api/Models/CategoryModel.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Runtime.Serialization;
 namespace PetShop_Api.Models
 {
     public class CategoryModel
@@ -8,6 +10,8 @@ namespace PetShop_Api.Models
         [Key]
         public long IdCategory {get; set;}
         public string Name {get;set;}
+        
+        [IgnoreDataMember]
         public List<ProductModel> Products {get;set;}
 
     }
--- PetShop-Api/Models/StateProductModel.cs
+++ PetShop-Api/Models/StateProductModel.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Runtime.Serialization;
 
 namespace PetShop_Api.Models
 {
@@ -8,6 +11,7 @@ namespace PetShop_Api.Models
         [Key]
         public long IdStateProduct {get; set;}
         public string Description {get;set;}
+        [IgnoreDataMember]
         public List<ProductModel> Products {get;set;}
 
     }
