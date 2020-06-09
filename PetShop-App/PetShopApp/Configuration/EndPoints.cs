d535ad4c798e5008599108ef7c333f86650d6e67
Tania Obando <taniaobando1998@gmail.com>
2020-06-09 11:10:52 -0500

    Se creo documento para endpoints

---
+++ PetShop-App/PetShopApp/Configuration/EndPoints.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShopApp.Configuration
+{
+    public class EndPoints
+    {
+        public static readonly string ULR_SERVIDOR = "";
+    }
+}
