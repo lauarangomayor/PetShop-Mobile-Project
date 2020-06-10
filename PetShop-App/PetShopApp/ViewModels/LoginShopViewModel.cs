5bb7aedb33297e749de5e5c62cfd9a2aa2c0304e
veronicatofino <veronicatofino@javerianacali.edu.co>
2020-06-10 00:51:26 -0500

    Validación Login con API, manejo errores

--- PetShop-App/PetShopApp/ViewModels/LoginShopViewModel.cs
+++ PetShop-App/PetShopApp/ViewModels/LoginShopViewModel.cs
@@ -98,7 +98,7 @@ namespace PetShopApp.ViewModels
                 //Exception e;
                 //UserDialogs.Instance.ShowError("El usuario no existe en el sistema", 4000);
                 //await Application.Current.MainPage.DisplayAlert("Error", "El usuario no existe en el sistema", "OK");
-                await Application.Current.MainPage.DisplayAlert("Error", EmailEntry +" " + PasswordEntry, "OK");
+                await Application.Current.MainPage.DisplayAlert("Error", "El correo no se encuentra en eñ sistema", "OK");
             };
             
         } 
