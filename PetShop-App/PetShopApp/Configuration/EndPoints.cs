2cb8b66b19c1c2512baf7232809f5f9985fcd844
Tania Obando <taniaobando1998@gmail.com>
2020-06-10 09:02:00 -0500

    Anexo endpoints

--- PetShop-App/PetShopApp/Configuration/EndPoints.cs
+++ PetShop-App/PetShopApp/Configuration/EndPoints.cs
@@ -15,6 +15,7 @@ namespace PetShopApp.Configuration
         public static readonly string GET_APPOINTMENTRECORDS = "appointmentrecord/getAppointmentRecordByPetId/";
         public static readonly string REGISTER_VET = "Veterinarian/registerVeterinarian";
         public static readonly string VALIDATE_CLIENT = "Client/getClientByEmailAndPassword/";
+        public static readonly string REGISTER_CLIENT = "client/registerClient";
 
     }
 }
