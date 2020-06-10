40af8ef8968c7d682a6d3a734b566da52e29416a
Tania Obando <taniaobando1998@gmail.com>
2020-06-10 10:02:59 -0500

    Eliminar mascota

--- PetShop-App/PetShopApp/ViewModels/HomeShopViewModel.cs
+++ PetShop-App/PetShopApp/ViewModels/HomeShopViewModel.cs
@@ -23,7 +23,7 @@ namespace PetShopApp.ViewModels
             SelectedItemTappedCommand = new Command(async () => await GoToProductDetails(), () => true);
             NavigationService.RemovePreviousPage();
             ProductsList = new ObservableCollection<ProductModel>();
-
+            /*
             ProductsList.Add(new ProductModel { ID = 1, Name = "Kit completo", Description="Incluye cepillos, cortauñas y un maletin.", UnitPrice = 53600,Category= new CategoryModel { IdCategory = 1,Name= "Aseo"}, Stock = 1, StateProduct = new StateProductModel { IdStateProduct =1, Description="Creado"},Image = "https://sc01.alicdn.com/kf/HTB1HZOiaLjsK1Rjy1Xaq6zispXaP.jpg" });
             ProductsList.Add(new ProductModel { ID = 2, Name = "Bowls de comida para perro", Description="Bolws metalicos para comida y agua", UnitPrice = 23000,Category= new CategoryModel { IdCategory = 2,Name= "Accesorios"}, Stock = 5, Image = "https://static2.abc.es/media/recreo/2019/02/21/Tiendanimal-plan-renove-mascotas-04-kExG--220x220@abc.PNG" });
             ProductsList.Add(new ProductModel { ID = 3, Name = "Kit limpieza petys", Description="Kit completo marca Petys para la liempieza de las necesidades de sus mascotas en el hogar.", UnitPrice = 5300,Category= new CategoryModel { IdCategory = 1,Name= "Aseo"}, Stock = 5, Image = "https://www.america-retail.com/static//2015/01/petys.png" });
@@ -31,7 +31,7 @@ namespace PetShopApp.ViewModels
             ProductsList.Add(new ProductModel { ID = 5, Name = "Collar de Smokin", Description="Collar en forma de smokin para gatos y perros.", UnitPrice = 1500,Category= new CategoryModel { IdCategory = 2, Name = "Accesorios" }, Stock = 2, Image = "https://ae01.alicdn.com/kf/H2d1daf13efa34b388de546682b18e5ceL/Collar-para-perro-y-gato-correa-ajustable-para-Collar-de-gato-accesorios-para-perros-mo-o.jpg" });
             ProductsList.Add(new ProductModel { ID = 14, Name = "Solución Advantix", Description="Solucion topca para perros de 10 kg a 25 kg", UnitPrice = 46600,Category= new CategoryModel { IdCategory = 3,Name= "Medicamentos"}, Stock = 8, Image = "https://mascotas.bayer.com.mx/static/media/images/upload/Productos/advantixgde-ectoparasiticida-ba13820-1.jpg" });
             
-            
+            */
 
         }
         #region Methods
