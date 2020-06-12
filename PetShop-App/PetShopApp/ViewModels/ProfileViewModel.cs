31f78d2cf2fc5672f13dfb166bf0395fc079b383
TheBoredJuan <juanlink@javerianacali.edu.co>
2020-06-12 09:37:23 -0500

    Creación del ProfileViewModel

---
+++ PetShop-App/PetShopApp/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,98 @@
+﻿using PetShopApp.Models;
+using PetShopApp.Services.APIRest;
+using PetShopApp.Views;
+using PetShopApp.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using PetShopApp.AuxModels;
+using Newtonsoft.Json;
+using System.IO;
+using PetShopApp.Helpers;
+
+namespace PetShopApp.ViewModels
+{
+    class ProfileViewModel : ViewModelBase
+    {
+        #region Properties
+        private List<OrderModel> orders;
+        private ObservableCollection<OrderModel> ordersList;
+        private UserModel userClient;
+        #endregion
+        #region Requests
+        public RequestPicker<BaseModel> GetOrders { get; set; }
+
+        public RequestPicker<BaseModel> GetUserClient { get; set; }
+        #endregion
+        #region Commands
+
+        #endregion
+        #region Getters/Setters
+        public List<OrderModel> Orders
+        {
+            get { return orders; }
+            set { orders = value; OnPropertyChanged(); }
+        }
+        public ObservableCollection<OrderModel> OrdersList
+        {
+            get { return ordersList; }
+            set { ordersList = value; OnPropertyChanged(); }
+        }
+        public UserModel UserClient
+        {
+            get { return userClient; }
+            set { userClient = value; OnPropertyChanged(); }
+        }
+        public ProfileViewModel()
+        {
+            Orders = new List<OrderModel>();
+            OrdersList = new ObservableCollection<OrderModel>();
+            InitizalizeRequest();
+            InitializeCommands();
+        }
+        #endregion
+        #region Methods
+        public void InitializeCommands()
+        {
+
+        }
+        public async void InitizalizeRequest()
+        {
+            string urlGetOrders = EndPoints.SERVER_URL + EndPoints.GET_ORDERS_BY_CLIENT + Settings.UId;
+            GetOrders = new RequestPicker<BaseModel>();
+            GetOrders.StrategyPicker("GET", urlGetOrders);
+            await ListOrders();
+
+            string urlGetClient = EndPoints.SERVER_URL + EndPoints.GET_USER_BY_CLIENT + Settings.UId;
+            GetUserClient = new RequestPicker<BaseModel>();
+            GetUserClient.StrategyPicker("GET", urlGetClient);
+            await UserClientGet();
+        }
+        public async Task UserClientGet() {
+            APIResponse response = await GetUserClient.ExecuteStrategy(null);
+            if (response.IsSuccess) {
+                var jsonSerializerSettings = new JsonSerializerSettings();
+                jsonSerializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
+                UserClient = JsonConvert.DeserializeObject<UserModel>(response.Response, jsonSerializerSettings);
+            }
+            
+        }
+        public async Task ListOrders() {
+            APIResponse response = await GetOrders.ExecuteStrategy(null);
+            if (response.IsSuccess) {
+                var jsonSerializerSettings = new JsonSerializerSettings();
+                jsonSerializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
+                Orders = JsonConvert.DeserializeObject<List<OrderModel>>(response.Response, jsonSerializerSettings);
+                foreach (var o in Orders) {
+                    OrdersList.Add(o);
+                }
+            }
+        }
+        #endregion
+
+    }
+}
--- PetShop-App/PetShopApp/Views/ProfileView.xaml.cs
+++ PetShop-App/PetShopApp/Views/ProfileView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetShopApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,11 @@ namespace PetShopApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfileView : ContentPage
     {
+        ProfileViewModel context = new ProfileViewModel();
         public ProfileView()
         {
             InitializeComponent();
+            BindingContext = context;
         }
     }
 }
